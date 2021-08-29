5926cdc5cd3d7fb3ba6261b11247c47b9b368f0b
Siemowit Baldyga <silbarad@gmail.com>
2021-08-29 22:01:20 +0200

    #10 Add analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
   configureWebpack: {
     performance: {
       maxEntrypointSize: 1024000,
       maxAssetSize: 1024000,
     },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: process.env.ANALYZER == '1'?'static':'none',
+      }),
+    ],
   },
 };
